d19037f88c95f4eca0ef8939aaf52c1708d14f26
Jacquelline Beltran <jacquelline500@gmail.com>
2019-02-25 23:44:19 -0800

    Add playRound function

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function computerPlay() {
   const options = ["rock", "paper", "scissors"];
   return options[Math.floor(Math.random() * Math.floor(2))];
 }
+
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay();
+  if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerPlay == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "rock")
+  ) {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+  } else if (playerSelection == computerSelection) {
+    console.log(`It's a draw`);
+  } else {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  }
+}
